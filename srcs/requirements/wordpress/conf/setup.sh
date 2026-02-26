d18ce8ae8f0537dc11ea0bc53f5320b02f533cd7
Nelson Figueiredo <matendafigueiredo@hotmail.com>
2026-02-26 23:20:14 +0100

    feat: add and config wordpress

---
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+: "${MYSQL_DB:?$MYSQL_DB NOT DEFINED}"
+: "${MYSQL_USER:?$MYSQL_USER NOT DEFINED}"
+: "${MYSQL_PWD:?$MYSQL_PWD NOT DEFINED}"
+: "${MYSQL_HOST:?$MYSQL_HOST NOT DEFINED}"
+: "${DOMAIN_NAME:?$DOMAIN_NAME NOT DEFINED}"
+: "${TITLE:?$TITLE NOT DEFINED}"
+: "${WP_ADM:?$WP_ADM NOT DEFINED}"
+: "${WP_ADM_PASS:?$WP_ADM_PASS NOT DEFINED}"
+: "${WP_ADM_EMAIL:?$WP_ADM_EMAIL NOT DEFINED}"
+: "${WP_USER_EMAIL:?$WP_USER_EMAIL NOT DEFINED}"
+: "${WP_USER_LOGIN:?$WP_USER_LOGIN NOT DEFINED}"
+: "${WP_USER_ROLE:?$WP_USER_ROLE NOT DEFINED}"
+: "${WP_USER_PASS:$WP_USER_PASS NOT DEFINED}"
+
+VOL_DIR=/var/www/wordpress
+if [ ! -f "$VOL_DIR/wp-config.php" ]; then
+	cd /var/www/wordpress
+	wp --allow-root core download
+	wp --allow-root config create \
+		--dbname=$MYSQL_DB \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PWD \
+		--dbhost=$MYSQL_HOST
+	wp --allow-root core install \
+    		--url=$DOMAIN_NAME \
+    		--title=$TITLE \
+    		--admin_user=$WP_ADM \
+    		--admin_password=$WP_ADM_PASS \
+    		--admin_email=$WP_ADM_EMAIL
+	wp --allow-root create "$WP_USER_LOGIN" "$WP_USER_EMAIL" \
+		--role="$WP_USER_ROLE" \
+		--user_pass="$WP_USER_PASS"
+fi 
+
+exec /usr/sbin/php-fpm83 -F
